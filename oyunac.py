749a50debd27994dfdd9dc07ad2939c5596d20e3
Mert Çobanoğlu <mertcobanov@gmail.com>
2018-05-03 22:26:05 +0300

    a

--- oyunac.py
+++
@@ -1,4 +0,0 @@
-import webbrowser
-
-def open_game(browser, url):
-    return webbrowser.get(browser).open(url)
--- yumrukoyunu.py
+++ yumrukoyunu.py
@@ -1,23 +1,28 @@
 import pyautogui
-from oyunac import*
+import webbrowser
 import cv2
 
 yumruk_cascade = cv2.CascadeClassifier('fist.xml')
 
-def tanimla(frame):
+
+def tanimla(goruntu):
     global det
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(goruntu, cv2.COLOR_BGR2GRAY)
     yumruk = yumruk_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in yumruk:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+        cv2.rectangle(goruntu, (x, y), (x + w, y + h), (255, 0, 0), 2)
         det = 1
-    return frame
+    return goruntu
+
+
+def open_game(browser, url):
+    return webbrowser.get(browser).open(url)
+
 
 video_capture = cv2.VideoCapture(0)
 
-open_game(browser='windows-default', url='http://apps.thecodepost.org/trex/trex.html')
+open_game(browser='windows-default',
+          url='http://apps.thecodepost.org/trex/trex.html')
 
 while True:
     _, frame = video_capture.read()
@@ -25,5 +30,5 @@ while True:
     canvas = tanimla(frame)
     cv2.imshow('Oyun', canvas)
     cv2.waitKey(1)
-    if (det == True):
+    if (det == 1):
         pyautogui.press("space")
